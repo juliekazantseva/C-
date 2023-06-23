6d67dac25f39b8946744c3702a3950f2a0e4ba6e
Julie Kazantseva <kazantsevayulya1503@gmail.com>
2023-06-23 12:51:52 +0300

    seminar1

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+Console.Write("Write the first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Write the second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB * numberB)
+{
+System.Console.WriteLine("Да");
+}
+else
+{
+System.Console.WriteLine("Нет");
+}
+
---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+
+Console.Write("Write the day of the week: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day>7 || day<1)
+{
+    Console.WriteLine ("There is no a weekday with this number");
+    return;
+}
+if (day == 1) Console.WriteLine ("Monday");
+if (day == 2) Console.WriteLine ("Tuesday");
+if (day == 3) Console.WriteLine ("Wednesday");
+if (day == 4) Console.WriteLine ("Thursday");
+if (day == 5) Console.WriteLine ("Friday");
+if (day == 6) Console.WriteLine ("Saturday");
+if (day == 7) Console.WriteLine ("Sunday");
