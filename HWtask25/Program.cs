55a6a329d1d9a6b667cc4b4212630dd031775a0e
Julie Kazantseva <kazantsevayulya1503@gmail.com>
2023-07-05 17:44:23 +0300

    HW 25, 27, 29 added

---
+++ HWtask25/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+System.Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+System.Console.Write($"{Math.Pow(numberA, numberB)}");
---
+++ HWtask27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Summ (int number)
+{
+    int sum = 0;
+    string UserNum = number.ToString();
+
+    for (int i = 0; i < UserNum.Length; i++)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Summ(number));
