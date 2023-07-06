13b56109f00bce9496d454cdc7945fc72021784f
Julie Kazantseva <kazantsevayulya1503@gmail.com>
2023-07-06 23:30:04 +0300

    Added HWTasks 41, 43

---
+++ HWtask41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] FillArrayWithRandomNumber(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{i + 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int AmountOfPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+System.Console.Write("Сколько чисел желаем ввести: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine($"Количество чисел больше нуля: {AmountOfPositiveNumbers(array)}");
---
+++ HWtask43/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух 
+// прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double Distance(double k1, double k2, double b1, double b2)
+{
+double distance = (b2 - b1) / (k1 - k2);
+return Math.Round(distance, 2);
+}
+System.Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Координатa пересечения прямых {Distance(k1, k2, b1, b2)}; {(k1 * Distance(k1, k2,b1, b2) + b1)}");
