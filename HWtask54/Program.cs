77abeff3277228b4b2c9290f2f6d4e2ec424d114
Julie Kazantseva <kazantsevayulya1503@gmail.com>
2023-07-09 21:46:14 +0300

    Added HWtasks 54, 56, 60

---
+++ HWtask54/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая 
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void FillMatrixWithRandom(int[,] matrix)
+{
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = rnd.Next(0, 10);
+}
+}
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+System.Console.Write($"{matrix[i, j]} \t");
+}
+System.Console.WriteLine();
+}
+}
+
+void SortElements(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int n = j + 1; n < matrix.GetLength(1); n++)
+            {
+                if (matrix[i, j] < matrix[i, n])
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, n];
+                    matrix[i, n] = temp;
+                }
+            }
+        }
+    }
+}
+
+System.Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row, column];
+FillMatrixWithRandom(matrix);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+SortElements(matrix);
+PrintMatrix(matrix);
+
---
+++ HWtask60/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся 
+// двузначных чисел. Напишите программу, которая будет построчно 
+// выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void FillMatrixWithRandom3D(int[,,] matrix)
+{
+    Random rnd = new Random();
+    int temp = 0;
+    int[] n = new int[matrix.GetLength(0) + matrix.GetLength(1) + matrix.GetLength(2)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int m = 0; m < matrix.GetLength(2); m++)
+            {
+                for (int l = 0; l < n.Length; l++)
+                {
+                    temp = rnd.Next(9,100);
+                    for (int t = 0; t < n.Length; t++)
+                        {
+                            if (n[t] == temp)
+                            {
+                                temp = rnd.Next(0, 10);
+                                t = 0;
+                            }
+                        }
+                    n[l] = temp;
+                    matrix[i, j, m] = temp;
+                }
+            }
+        }
+    }
+    System.Console.WriteLine(string.Join(";", n));
+}
+
+
+void PrintMatrix3D(int[,,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+for (int m = 0; m < matrix.GetLength(2); m++)
+    {
+        System.Console.Write($"{matrix[i, j, m]} ({i}, {j}, {m}); \t");
+    }
+System.Console.WriteLine();
+}
+}
+}
+
+System.Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во страниц: ");
+int page = Convert.ToInt32(Console.ReadLine());
+int[,,] matrix = new int[row, column, page];
+FillMatrixWithRandom3D(matrix);
+PrintMatrix3D(matrix);
